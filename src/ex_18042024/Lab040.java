f1ca1d8b72e440de222e9324c9c077e646b691fc
Abhijeet-Mahapatra <abhijeetmahapatra305@gmail.com>
2024-04-18 15:59:18 +0530

    180424 Push

---
+++ src/ex_18042024/Lab040.java
@@ -0,0 +1,11 @@
+package ex_18042024;
+
+public class Lab040 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        // byte c = a+b; // invalid code need to do type casting //byte c = byte(a+b);
+
+        System.out.println(a+b);
+    }
+}
